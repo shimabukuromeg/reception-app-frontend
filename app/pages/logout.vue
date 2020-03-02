4f638abb83994102725fe62baf75b7541c618b0d
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-03-03 08:17:43 +0900

    ログアウト追加

---
+++ app/pages/logout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app flex-row align-items-center">
+    <div class="container">
+    </div>
+  </div>
+</template>
+
+<script>
+  import Cookies from 'universal-cookie'
+  export default {
+    mounted () {
+      this.$store.dispatch('user/logout')
+      const cookies = new Cookies()
+      cookies.remove('currentUser')
+      this.$router.replace('/login')
+    },
+  }
+</script>
