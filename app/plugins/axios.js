b687136bdc7342aba6536021ab2b0f2d0f1794bd
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-03-16 20:11:42 +0900

    headerにAuthorizationが付与されるように修正

--- app/plugins/axios.js
+++ app/plugins/axios.js
@@ -1,5 +1,19 @@
-import axios from 'axios'
+import Cookies from 'universal-cookie'
 
-export default axios.create({
-  baseURL: process.env.API_URL
-})
+export default function ({ $axios, redirect }) {
+
+  $axios.setBaseURL(process.env.API_URL)
+
+  $axios.onRequest(config => {
+
+    // APIキーが取得済みの場合
+    const cookies = new Cookies()
+    const accessToken = cookies.get('accessToken')
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`
+    }
+
+    console.log('Making request to ' + config.url)
+  })
+}
