efd70fd514858e0f8819ddf3f73349dce40b9d6a
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-03-16 22:42:22 +0900

    不要な条件判定を省いた

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -5,15 +5,11 @@ export default async ({ req, store, redirect }) => {
   if (process.client){
     return
   }
-
-  if(req && req.headers && req.headers.cookie){
-    // your code
-    const cookies = new Cookies(req.headers.cookie)
-    const currentUser = cookies.get('currentUser')
-    if (currentUser && currentUser.id ){
-      store.commit('user/setCurrentUser', currentUser, { root: true })
-    } else {
-      return redirect('/login')
-    }
+  const cookies = new Cookies(req.headers.cookie)
+  const currentUser = cookies.get('currentUser')
+  if (currentUser && currentUser.id ){
+    store.commit('user/setCurrentUser', currentUser, { root: true })
+  } else {
+    redirect('/login')
   }
 }
