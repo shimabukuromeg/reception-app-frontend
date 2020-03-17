cd52d73d7f473b0da1346f7cc670b84088cf1dc4
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2020-03-17 16:17:32 +0900

    Cannot read property Headers of undefinedが直らなかったので修正

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -7,16 +7,14 @@ export default async ({ req, store, redirect }) => {
   }
 
   if(req && req.headers && req.headers.cookie){
-    return
-  }
-
-  const cookies = new Cookies(req.headers.cookie)
-  const currentUser = cookies.get('currentUser')
-  const accessToken = cookies.get('accessToken')
-  if (accessToken && currentUser && currentUser.id ){
-    store.commit('user/setCurrentUser', currentUser)
-    store.commit('user/setAccessToken', accessToken)
-  } else {
-    redirect('/login')
+    const cookies = new Cookies(req.headers.cookie)
+    const currentUser = cookies.get('currentUser')
+    const accessToken = cookies.get('accessToken')
+    if (accessToken && currentUser && currentUser.id ){
+      store.commit('user/setCurrentUser', currentUser)
+      store.commit('user/setAccessToken', accessToken)
+    } else {
+      redirect('/login')
+    }
   }
 }
